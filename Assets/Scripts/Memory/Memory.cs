eb9e5ac023b718fddf794d4c9d55d198a2db3577
Your Name <yereminsasha@gmail.com>
2025-07-13 16:30:19 -0700

    mono

--- Assets/Scripts/Memory/Memory.cs
+++ Assets/Scripts/Memory/Memory.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class NeedsSystem : MonoBehaviour
+public class Memory : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
