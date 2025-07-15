a9983a6cc6a5868ec96d9fabb8791ccf03bbf4fb
Your Name <yereminsasha@gmail.com>
2025-07-15 06:48:02 -0700

    PAD brainstorming

--- Assets/Scripts/Emotion/PADState.cs
+++ Assets/Scripts/Emotion/PADState.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
 
-public class PADState : MonoBehaviour
+public class PADState
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private float _pleasure;
+    private float _arousal;
+    private float _dominance;
+//ro
+    public float Pleasure => _pleasure;
+    public float Arousal => _arousal;
+
+    public float Dominance => _dominance;
+
+    
+    public PADState(float pleasure = 0f, float arousal = 0f, float dominance = 0f)
     {
-        
+        _pleasure = pleasure;
+        _arousal = arousal;
+        _dominance = dominance;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    // basic arithmetic for now
+    public void ApplyDelta(float dPleasure, float dArousal, float dDominance)
     {
-        
+        _pleasure += dPleasure;
+        _arousal += dArousal;
+        _dominance += dDominance;
+
+    }
+
+    // option to reset if needed
+    public void Reset()
+    {
+        _pleasure = 0f;
+        _arousal = 0f;
+        _dominance = 0f;
+
     }
 }
