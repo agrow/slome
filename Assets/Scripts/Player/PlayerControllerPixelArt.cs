bf0b48583fd7ca9c401769391eb7c742d6e19d79
agrow <grow.april@gmail.com>
2025-07-30 12:45:24 -0700

    added 8D idle and walking animations using blendtrees

--- Assets/Scripts/Player/PlayerControllerPixelArt.cs
+++ Assets/Scripts/Player/PlayerControllerPixelArt.cs
@@ -5,12 +5,14 @@ public class PlayerControllerPixelArt : MonoBehaviour
 {
     public float speed; // modulates movement
     private Vector2 move; // input system values, -1 to 1
+    private Vector2 lastMove; // Used to save animator state to last direction when player stops moving
     //public float groundDist; // For if we want elevated ground/slopes w/o jumping (https://www.youtube.com/watch?v=cqNBA9Pslg8)
     //public LayerMask terrainLayer;
 
     public Rigidbody rb;
     public SpriteRenderer sr;
     public Camera cam;
+    public Animator anim;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -38,7 +40,35 @@ public class PlayerControllerPixelArt : MonoBehaviour
 
         */
 
+        
+    }
+
+    // After camera's moved
+    void LateUpdate()
+    {
         movePlayerRelativeToCamera();
+
+        // Time to deal with animations. First, determine if we are idle or moving
+        if (move.x != 0 || move.y != 0)
+        {
+            // If we are MOVING, save it as our last movement direction
+            lastMove = new Vector2(move.x, move.y);
+            anim.SetBool("isMoving", true);
+
+        }
+        else
+        {
+            // If we are NOT MOVING, play the idle animation facing the last direction we were moving
+            // Note: moveX & moveY should be ZERO if we are not moving, so we should be in the idle blendtree if that's true
+            anim.SetBool("isMoving", false);
+        }
+
+        // Does the blendtree handle diagonals automatically?
+        // Sort of... really hard to idle facing a diagonal. 
+        // TODO: Test with joystick, refigure values
+        anim.SetFloat("lastMoveX", lastMove.x);
+        anim.SetFloat("lastMoveY", lastMove.y);
+
     }
 
     public void OnMove(InputAction.CallbackContext context)
