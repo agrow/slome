33d32a8b532a2ea85e5e4e4e36b6a494a2fa6e2f
agrow <grow.april@gmail.com>
2025-08-14 11:18:40 -0700

    fixing player for main branch and replacing models I thought I put in last commit

--- Assets/Scripts/Player/PlayerControllerPixelArt.cs
+++ Assets/Scripts/Player/PlayerControllerPixelArt.cs
@@ -9,9 +9,9 @@ public class PlayerControllerPixelArt : MonoBehaviour
     //public float groundDist; // For if we want elevated ground/slopes w/o jumping (https://www.youtube.com/watch?v=cqNBA9Pslg8)
     //public LayerMask terrainLayer;
     private Vector3 lastWorldSpaceMove = new Vector3(0, 0, -1); // Face the camera(?) at start
-    public float walkableRaycastHeight = 2f; // The distance above the rb.position to start the vertical movement raycast calculation
+    public float walkableRaycastHeight = 1f; // The distance above the rb.position to start the vertical movement raycast calculation
     public LayerMask walkableMask; // Assign in Inspector to include all walkable surfaces
-    public float bodyHeightOffset = 0; // Distance from the collider center and the floor
+    public float bodyHeightOffset = .24f; // Distance from the collider center and the floor
 
     public Rigidbody rb;
     public SpriteRenderer sr;
