7d4cf59d4b817df859cd81d8a2376cf31958a949
Joaquin Arredondo <joaquinanthonyarredondo@gmail.com>
2025-08-28 02:09:57 -0700

    saved changes, needed to pull main

--- Assets/Scripts/Emotion/Personality_01.cs/PersonalityTypeDefinition.cs
+++ Assets/Scripts/Emotion/Personality_01.cs/PersonalityTypeDefinition.cs
@@ -19,7 +19,30 @@ public class PersonalityTypeDefinition : ScriptableObject
     [Range(0, 1)] public float structure;
 
     public string stressResponse;
-    public string primaryLoveLanguage;
+
+
+
+    /*
+
+
+    */ 
+
+    // Brainstorm how to use these for considerations? 
+    public string primaryLoveLanguage; // e.g., Words of Affirmation, Acts of Service, Receiving Gifts, Quality Time, Physical Touch, a consideration 
+    /* Example distributions of considerations: for Love Language 
+        Physical Touch: 1.0 
+        Words of Affirmation: 8.0 
+        Quality Time: 3.0
+        Acts of Service: 6.0
+        Receiving Gifts: 2.0 
+
+        Touch: if i am feeling low pleasure, and someone touches me, i get a boost to pleasure. 
+            - Physical touch? 1.0 
+            - Intent Nudge (Hug, Hold Hands, Kiss, Cuddle): Light Nudge + 
+            - Consideration of PAD
+            - Consideration of PAD Delta
+            - Consideration of Relationship Status 
+    */ 
     public string attachmentStyle;
 
     [Range(0, 1)] public float pleasureBaseline;
