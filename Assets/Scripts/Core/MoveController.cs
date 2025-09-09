3d78be22632a33f2194fcba8f265b47d2c1f5827
Your Name <yereminsasha@gmail.com>
2025-09-08 21:55:24 -0700

    working on adding npcs to final scene

--- Assets/Scripts/Core/MoveController.cs
+++ Assets/Scripts/Core/MoveController.cs
@@ -1,26 +1,56 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 namespace TL.Core
+
 {
-    //Purpose Statement: Moving NPC around the worldspace
+    // Purpose Statement: Moving NPC around the worldspace and animating sprite
     public class MoveController : MonoBehaviour
     {
-        private NavMeshAgent agent;
-        public Transform destination;
+        [SerializeField] private NavMeshAgent agent;
+        [SerializeField] private Animator anim;
 
-        // Start is called before the first frame update
+        // no need for rigid body because of the nav mesh
+        private Vector2 lastMove = new Vector2(0, -1); // Default facing down
+
         void Start()
         {
-            agent = GetComponent<NavMeshAgent>();
+            // Only assign if not set in Inspector
+            if (agent == null) agent = GetComponent<NavMeshAgent>();
+            if (anim == null) anim = GetComponent<Animator>();
         }
 
-        // Update is called once per frame
         void Update()
         {
-            
+            Vector3 velocity = agent.velocity;
+            bool isMoving = velocity.magnitude > 0.05f;
+
+            // Set isMoving parameter for animator
+            if (anim != null)
+                anim.SetBool("isMoving", isMoving);
+
+            // If moving, update lastMove direction for idle facing
+            if (isMoving)
+            {
+                lastMove = new Vector2(velocity.normalized.x, velocity.normalized.z);
+
+                if (anim != null)
+                {
+                    anim.SetFloat("lastMoveX", lastMove.x);
+                    anim.SetFloat("lastMoveY", lastMove.y);
+                }
+            }
+            else
+            {
+                // When idle, keep lastMoveX/Y as last direction
+                if (anim != null)
+                {
+                    anim.SetFloat("lastMoveX", lastMove.x);
+                    anim.SetFloat("lastMoveY", lastMove.y);
+                }
+            }
         }
 
         public void MoveTo(Vector3 position)
